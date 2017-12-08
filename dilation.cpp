f8add379a2d45c5e4ff4a05c049953dc615c5906
Krushan Bauva <krushan@bauva.com>
2017-12-09 02:25:15 +0530

    Add a program to implement dilation on an image

---
+++ dilation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int rows,cols;
+
+int isValid(int x , int y) {
+	if((x>=0)&&(y>=0)&&(x<rows)&&(y<cols)) {
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	int flag;
+	Mat img = imread(argv[1],0);
+	rows = img.rows , cols = img.cols;
+	Mat n_img(img.rows,img.cols,CV_8UC1,Scalar(0));
+	int X[] = {-1,-1,-1,0,0,0,1,1,1};
+	int Y[] = {-1,0,1,-1,0,1,-1,0,1};
+	for(int i=0 ; i<img.rows ; i++) {
+		for(int j=0 ; j<img.cols ; j++) {
+			flag = 0;
+			for(int k=0 ; k<9 ; k++) {
+				if(isValid(i+X[k],j+Y[k])) {
+					if(img.at<uchar>(i+X[k],j+Y[k])==255) {
+						flag = 1;
+					}
+				}
+			}
+			if(flag==1) {
+				n_img.at<uchar>(i,j)=255;
+			}
+		}
+	}
+	namedWindow("Dilation",WINDOW_NORMAL);
+	imshow("Dilation",n_img);
+	waitKey(0);
+	return 0;	
+}
